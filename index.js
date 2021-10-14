b97a2543eadfc8a5bffc2b96a0e69205d54c7274
rajendra kumar <rajendrakumar2770@gmail.com>
2021-10-14 23:44:23 +0530

    structure added

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+app.use(bodyParser.json({
+    extended: true
+}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
