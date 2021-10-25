cdaff8a222f76af20541e362821ff4086b850874
rajendra kumar <rajendrakumar2770@gmail.com>
2021-10-26 03:41:45 +0530

    signup conflicts resolved

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,18 +1,16 @@
 import express from 'express';
 import { signUp, logIn, getAllUser } from '../controller/user.js';
-
-import { signUp, logIn,getAllUser } from '../controller/user.js';
 import { enquiryMail } from '../controller/contact.js';
 const route = express.Router();
 
 route.post('/signup', signUp);
 route.get('/users', getAllUser);
 route.get('/login',logIn)
-route.post('/conversation/add', newConversation);
-route.post('/conversation/get', getConversation);
+// route.post('/conversation/add', newConversation);
+// route.post('/conversation/get', getConversation);
 route.post('/enqMail', enquiryMail);
 
-route.post('/message/add', newMessage);
-route.get('/message/get/:id', getMessage);
+// route.post('/message/add', newMessage);
+// route.get('/message/get/:id', getMessage);
 
 export default route;
