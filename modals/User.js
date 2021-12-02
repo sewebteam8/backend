c3d262d61390c3c463b956c64b6a9e9883c2ddc9
rajendra kumar <rajendrakumar2770@gmail.com>
2021-12-02 19:53:28 +0530

    updated

--- modals/User.js
+++ modals/User.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    collagename: {
+    collageName: {
         type: String,
         required: true
     },
